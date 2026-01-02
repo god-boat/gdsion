0a3691def17ad9c25462fe625984b387741ea0a2
Weston <godboatt@gmail.com>
2026-01-02 12:53:41 -0800

    new fade and pitch time modes to prevent clicking when chaging delay time, pitch works good, fade doesnt really work yet

--- src/effector/effects/si_effect_stereo_delay.cpp
+++ src/effector/effects/si_effect_stereo_delay.cpp
@@ -8,19 +8,35 @@
 
 #include "chip/siopm_ref_table.h"
 
-void SiEffectStereoDelay::set_params(double p_delay_time, double p_feedback, bool p_cross, double p_wet) {
+void SiEffectStereoDelay::set_params(double p_delay_time, double p_feedback, bool p_cross, double p_wet, int p_time_mode) {
 	double samples_per_ms = 48.0;
+	double sampling_rate = 48000.0;
 	SiOPMRefTable *ref_table = SiOPMRefTable::get_instance();
 	if (ref_table && ref_table->sampling_rate > 0) {
-		samples_per_ms = ref_table->sampling_rate / 1000.0;
+		sampling_rate = ref_table->sampling_rate;
+		samples_per_ms = sampling_rate / 1000.0;
 	}
 
 	int offset = (int)(p_delay_time * samples_per_ms);
 	if (offset > DELAY_BUFFER_FILTER) {
 		offset = DELAY_BUFFER_FILTER;
 	}
-
-	_pointer_write = (_pointer_read + offset) & DELAY_BUFFER_FILTER;
+	int current_offset = (_pointer_write - _pointer_read) & DELAY_BUFFER_FILTER;
+	
+	_time_mode = static_cast<DelayTimeMode>(p_time_mode);
+	
+	if (current_offset != offset) {
+		_pointer_read_old = _pointer_read;
+		_pointer_read_target = (_pointer_write - offset) & DELAY_BUFFER_FILTER;
+		_pointer_read_fractional = 0.0;
+		_crossfade_position = 0.0;
+		int crossfade_samples = (int)(CROSSFADE_TIME * sampling_rate);
+		if (crossfade_samples > 0) {
+			_crossfade_increment = 1.0 / crossfade_samples;
+		} else {
+			_crossfade_increment = 1.0;
+		}
+	}
 
 	_feedback = p_feedback;
 	if (_feedback >= 1) {
@@ -43,9 +59,17 @@ int SiEffectStereoDelay::prepare_process() {
 }
 
 void SiEffectStereoDelay::_process_channel(Vector<double> *r_buffer, int p_buffer_index, Vector<double> *p_read_buffer, Vector<double> *r_write_buffer) {
-	double value = (*p_read_buffer)[_pointer_read];
+	double value;
+	
+	if (_crossfade_position < 1.0 && _time_mode == DELAY_TIME_MODE_FADE) {
+		double old_value = (*p_read_buffer)[_pointer_read_old];
+		double new_value = (*p_read_buffer)[_pointer_read_target];
+		value = old_value * (1.0 - _crossfade_position) + new_value * _crossfade_position;
+	} else {
+		value = (*p_read_buffer)[_pointer_read];
+	}
+	
 	r_write_buffer->write[_pointer_write] = (*r_buffer)[p_buffer_index] - value * _feedback;
-
 	r_buffer->write[p_buffer_index] = (*r_buffer)[p_buffer_index] * _dry_gain + value * _wet_gain;
 }
 
@@ -58,7 +82,33 @@ int SiEffectStereoDelay::process(int p_channels, Vector<double> *r_buffer, int p
 		_process_channel(r_buffer, i + 1, (_cross ? &_delay_buffer_left : &_delay_buffer_right), &_delay_buffer_right);
 
 		_pointer_write = (_pointer_write + 1) & DELAY_BUFFER_FILTER;
-		_pointer_read  = (_pointer_read  + 1) & DELAY_BUFFER_FILTER;
+		
+		if (_crossfade_position < 1.0) {
+			_crossfade_position += _crossfade_increment;
+			if (_crossfade_position >= 1.0) {
+				_crossfade_position = 1.0;
+				_pointer_read = _pointer_read_target;
+				_pointer_read_fractional = 0.0;
+			}
+			
+			if (_time_mode == DELAY_TIME_MODE_PITCH) {
+				int distance = (_pointer_read_target - _pointer_read_old) & DELAY_BUFFER_FILTER;
+				if (distance > (DELAY_BUFFER_FILTER >> 1)) {
+					distance = distance - DELAY_BUFFER_FILTER - 1;
+				}
+				double step = 1.0 + distance * _crossfade_increment;
+				_pointer_read_fractional += step;
+				
+				int step_int = (int)_pointer_read_fractional;
+				_pointer_read_fractional -= step_int;
+				_pointer_read = (_pointer_read + step_int) & DELAY_BUFFER_FILTER;
+			}
+			
+			_pointer_read_old = (_pointer_read_old + 1) & DELAY_BUFFER_FILTER;
+			_pointer_read_target = (_pointer_read_target + 1) & DELAY_BUFFER_FILTER;
+		} else {
+			_pointer_read = (_pointer_read + 1) & DELAY_BUFFER_FILTER;
+		}
 	}
 
 	return p_channels;
@@ -69,8 +119,9 @@ void SiEffectStereoDelay::set_by_mml(Vector<double> p_args) {
 	double feedback   = _get_mml_arg(p_args, 1, 25) / 100.0;
 	int cross         = _get_mml_arg(p_args, 2, 0);
 	double wet        = _get_mml_arg(p_args, 3, 100) / 100.0;
+	int time_mode     = _get_mml_arg(p_args, 4, DELAY_TIME_MODE_FADE);
 
-	set_params(delay_time, feedback, cross == 1, wet);
+	set_params(delay_time, feedback, cross == 1, wet, time_mode);
 }
 
 void SiEffectStereoDelay::reset() {
@@ -78,13 +129,16 @@ void SiEffectStereoDelay::reset() {
 }
 
 void SiEffectStereoDelay::_bind_methods() {
-	ClassDB::bind_method(D_METHOD("set_params", "delay_time", "feedback", "is_cross", "wet"), &SiEffectStereoDelay::set_params, DEFVAL(250), DEFVAL(0.25), DEFVAL(false), DEFVAL(0.25));
+	ClassDB::bind_method(D_METHOD("set_params", "delay_time", "feedback", "is_cross", "wet", "time_mode"), &SiEffectStereoDelay::set_params, DEFVAL(250), DEFVAL(0.25), DEFVAL(false), DEFVAL(0.25), DEFVAL(DELAY_TIME_MODE_FADE));
+	
+	BIND_ENUM_CONSTANT(DELAY_TIME_MODE_PITCH);
+	BIND_ENUM_CONSTANT(DELAY_TIME_MODE_FADE);
 }
 
-SiEffectStereoDelay::SiEffectStereoDelay(double p_delay_time, double p_feedback, bool p_cross, double p_wet) :
+SiEffectStereoDelay::SiEffectStereoDelay(double p_delay_time, double p_feedback, bool p_cross, double p_wet, int p_time_mode) :
 		SiEffectBase() {
 	_delay_buffer_left.resize_zeroed(1 << DELAY_BUFFER_BITS);
 	_delay_buffer_right.resize_zeroed(1 << DELAY_BUFFER_BITS);
 
-	set_params(p_delay_time, p_feedback, p_cross, p_wet);
+	set_params(p_delay_time, p_feedback, p_cross, p_wet, p_time_mode);
 }
--- src/effector/effects/si_effect_stereo_delay.h
+++ src/effector/effects/si_effect_stereo_delay.h
@@ -18,16 +18,30 @@ class SiEffectStereoDelay : public SiEffectBase {
 	static const int DELAY_BUFFER_BITS = 16;
 	static const int DELAY_BUFFER_FILTER = (1 << DELAY_BUFFER_BITS) - 1;
 
+public:
+	enum DelayTimeMode {
+		DELAY_TIME_MODE_PITCH,
+		DELAY_TIME_MODE_FADE
+	};
+
+private:
 	Vector<double> _delay_buffer_left;
 	Vector<double> _delay_buffer_right;
 
 	int _pointer_read = 0;
 	int _pointer_write = 0;
+	int _pointer_read_target = 0;
+	int _pointer_read_old = 0;
+	double _pointer_read_fractional = 0.0;
 	double _feedback = 0;
 	double _wet = 0;
 	double _dry_gain = 1.0;
 	double _wet_gain = 0.0;
 	bool _cross = false;
+	DelayTimeMode _time_mode = DELAY_TIME_MODE_FADE;
+	double _crossfade_position = 1.0;
+	double _crossfade_increment = 0.0;
+	static constexpr double CROSSFADE_TIME = 0.05;
 
 	void _process_channel(Vector<double> *r_buffer, int p_buffer_index, Vector<double> *p_read_buffer, Vector<double> *r_write_buffer);
 
@@ -35,7 +49,7 @@ protected:
 	static void _bind_methods();
 
 public:
-	void set_params(double p_delay_time = 250, double p_feedback = 0.25, bool p_cross = false, double p_wet = 0.25);
+	void set_params(double p_delay_time = 250, double p_feedback = 0.25, bool p_cross = false, double p_wet = 0.25, int p_time_mode = DELAY_TIME_MODE_FADE);
 
 	//
 
@@ -45,8 +59,10 @@ public:
 	virtual void set_by_mml(Vector<double> p_args) override;
 	virtual void reset() override;
 
-	SiEffectStereoDelay(double p_delay_time = 250, double p_feedback = 0.25, bool p_cross = false, double p_wet = 0.25);
+	SiEffectStereoDelay(double p_delay_time = 250, double p_feedback = 0.25, bool p_cross = false, double p_wet = 0.25, int p_time_mode = DELAY_TIME_MODE_FADE);
 	~SiEffectStereoDelay() {}
 };
+
+VARIANT_ENUM_CAST(SiEffectStereoDelay::DelayTimeMode);
 
 #endif // SI_EFFECT_STEREO_DELAY_H
